862a78bd036cf43444388e6bddd7eac3070df344
Wendel <wendel4019@gmail.com>
2024-03-23 17:19:34 -0300

    Created reactive form

---
+++ src/app/product.ts
@@ -0,0 +1,6 @@
+export interface Product { // indtroduzido dentro com componente products.ts
+    id: number,
+    name: string,
+    price: number,
+    quantity: number
+}
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Product } from '../product'; // import da interface Product de product.ts
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent { // criado componente recebendo um array vazio
+  products: Product[] = [];
+
+  formGroupProduct: FormGroup; // iniciando o FormGroup
+
+  constructor(private formBuilder: FormBuilder) { // construtor usando formBuilder
+    this.formGroupProduct = formBuilder.group({
+
+      id       : ['0007'], // criando um formGroup inicialmente vazio ['']
+      name     : ['Wendel'],
+      price    : ['12.99'],
+      quantity : ['41']
+    });
+  }
 
 }
